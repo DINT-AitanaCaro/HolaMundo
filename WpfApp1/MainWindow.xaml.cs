74186f9c656b00d752e3fb5d831ad40c5c317cda
AitanaCaro <aitana.caro@iesdoctorbalmis.com>
2022-09-23 12:49:42 +0200

    Primera versión

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void saludarButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (nombreTextBox.Text.Length == 0)
+                saludoTextBlock.Text = "Hola mundo!";
+            else
+                saludoTextBlock.Text = $"Hola {nombreTextBox.Text}!";
+        }
+    }
+}
